6174c35163f1093cef02e3da952b3c2539a9fd49
Vjg9 <vojin09jov@gmail.com>
2022-11-12 20:36:38 +0100

    Added init function to connect to db or create db

---
+++ src/db.rs
@@ -0,0 +1,20 @@
+use rusqlite::{Connection, Result, Error};
+
+// Return connection
+pub fn init(path: &str) -> Result<Connection, Error> {
+    let conn = connect_db(path)?;
+    Ok(conn)
+}
+
+// Connect to db if exists or create db with tables
+fn connect_db(path: &str) -> Result<Connection, Error> {
+    let conn = Connection::open(path)?;
+    conn.execute("
+        CREATE TABLE IF NOT EXISTS stack {
+            id      INTEGER PRIMARY KEY,
+            name    TEXT NOT NULL,
+        }",
+        (),
+    )?;
+    Ok(conn)
+}
