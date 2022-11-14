e8e61f1c759beeebdbff7dd1909e3fb4ae511334
Vjg9 <vojin09jov@gmail.com>
2022-11-14 19:13:14 +0100

    Added ON DELETE to card model

--- src/db.rs
+++ src/db.rs
@@ -16,6 +16,6 @@ pub fn init(path: &str) -> Result<Connection, rusqlite::Error> {
 fn connect_db(path: &str) -> Result<Connection, Error> {
     let conn = Connection::open(path)?;
     conn.execute("CREATE TABLE IF NOT EXISTS stack (id    INTEGER PRIMARY KEY, name    TEXT NOT NULL)", ())?;
-    conn.execute("CREATE TABLE IF NOT EXISTS card (id    INTEGER PRIMARY KEY, title    TEXT NOT NULL, text    TEXT NOT NULL, stack_id   INTEGER NOT NULL, FOREIGN KEY(stack_id) REFERENCES stack(id))", ())?;
+    conn.execute("CREATE TABLE IF NOT EXISTS card (id    INTEGER PRIMARY KEY, title    TEXT NOT NULL, text    TEXT NOT NULL, stack_id   INTEGER NOT NULL, FOREIGN KEY(stack_id) REFERENCES stack(id) ON DELETE CASCADE)", ())?;
     Ok(conn)
 }
