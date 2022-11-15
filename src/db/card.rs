d338a344b10282ed9a0276195c8dbc6d61b41ae9
Vjg9 <vojin09jov@gmail.com>
2022-11-15 18:57:27 +0100

    Added add query to the cards module

--- src/db/card.rs
+++ src/db/card.rs
@@ -7,3 +7,17 @@ pub struct Card {
     pub text: String,
     pub stack_id: i32,
 }
+
+pub fn add(conn: &Connection, stack_id: i32, title: String, text: String) {
+    let card = Card {
+        id: 0,
+        title: title,
+        text: text,
+        stack_id: stack_id,
+    };
+
+    conn.execute(
+        "INSERT INTO card (title, text, stack_id) VALUES (?1, ?2, ?3)",
+        (&card.title, &card.text, &card.stack_id)
+    ).unwrap();
+}
