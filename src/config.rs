408c0c46918a5273b626b224d9cc935dba6eb05c
Vjg9 <vojin09jov@gmail.com>
2022-11-27 13:14:13 +0100

    Added highlight_color to the config

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,7 @@ const CONFIG_DIR: &str = ".config/cards/";
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub db_file: String,
+    pub highlight_color: u8,
 }
 
 pub fn init() {
@@ -16,7 +17,7 @@ pub fn init() {
     } else {
         fs::create_dir(format!("{}/{}", home_dir, CONFIG_DIR)).unwrap();
         let mut file = fs::File::create(format!("{}/{}{}", home_dir, CONFIG_DIR, "config.json")).unwrap();
-        file.write(b"{\n \"db_file\": \".config/cards/cards.db\"\n}").unwrap();
+        file.write(b"{\n \"db_file\": \".config/cards/cards.db\",\n \"highlight_color\": 4\n}").unwrap();
     }
 }
 
@@ -26,4 +27,33 @@ pub fn get_db_file() -> String {
     let config: Config = serde_json::from_str(json.as_str()).unwrap(); 
 
     format!("{}/{}", &home_dir, &config.db_file)
+}
+
+pub fn get_db_file_raw() -> String {
+    let home_dir: String = env::var("HOME").unwrap();
+    let json: String = fs::read_to_string(format!("{}/{}{}", home_dir, CONFIG_DIR, "config.json")).unwrap();
+    let config: Config = serde_json::from_str(json.as_str()).unwrap(); 
+    
+    config.db_file
+}
+
+pub fn get_highlight_color() -> u8 {
+    let home_dir: String = env::var("HOME").unwrap();
+    let json: String = fs::read_to_string(format!("{}/{}{}", home_dir, CONFIG_DIR, "config.json")).unwrap();
+    let config: Config = serde_json::from_str(json.as_str()).unwrap(); 
+
+    config.highlight_color
+}
+
+pub fn set_config(db_file: String, highlight_color: u8) {
+   let config = Config {
+       db_file,
+       highlight_color,
+   }; 
+
+   let home_dir: String = env::var("HOME").unwrap();
+   let json = serde_json::to_string_pretty(&config).unwrap();
+   fs::remove_file(format!("{}/{}{}", home_dir, CONFIG_DIR, "config.json")).unwrap();
+   let mut file = fs::File::create(format!("{}/{}{}", home_dir, CONFIG_DIR, "config.json")).unwrap();
+   file.write(json.as_bytes()).unwrap();
 }
